32094643c22e2f5b4118968fb9e189d728b6af66
Truija99 <truijam@gmail.com>
2018-10-11 15:40:45 +0300

    Lisatud inkremendi funktsioon, vaja muuta list itemiteks saadud vastused.

--- src/components/Faktid.vue
+++ src/components/Faktid.vue
@@ -3,8 +3,11 @@
     <h1>Numbri faktid</h1>
     <p>Huvitavad faktid iga numbri kohta</p>
     <input type="number" min="0" v-model="number">
+    <input type="number" v-model="inc">
     <p>{{ data }}</p>
-
+    <ul>
+      <li></li>
+    </ul>
   </div>
 </template>
 
@@ -15,22 +18,35 @@ export default {
   data () {
     return {
       data: '',
-      number: 0
+      number: 0,
+      inc:0,
+      counter: '',
     }
   },
-  watch: {
-    number() {
-      if (this.number !== '' && this.number !== '') {
-      let url = `http://numbersapi.com/${this.number}`
+  methods: {
+      //number () {}  on ka method
+      getFact() {
+        if (this.number !== '' && this.inc !== '') {
+      let url = `http://numbersapi.com/`;
+      let secondUrl = `${url}${this.number}..${parseInt(this.number) + parseInt(this.inc)}`;
       
-      axios.get(url)
+      axios.get(secondUrl)
       .then(response => {
         this.data = response.data
       })
       .catch(error => {
-        console.log(error)
+          console.log(error)
       })
       }
+      }
+  },
+  watch: { /*kutsub esile*/
+    number() {
+      this.getFact()
+    },
+   
+    inc() {
+      this.getFact()
     }
   }
 }
@@ -38,5 +54,5 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
- 
+    
 </style>
