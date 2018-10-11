a822b34c8915b8ee3c73ac4606d3a129363ab80c
Truija99 <truijam@gmail.com>
2018-10-11 13:56:26 +0300

    Lisatud numbri faktide funktsionaalsus.

---
+++ src/components/Faktid.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    <h1>Numbri faktid</h1>
+    <p>Huvitavad faktid iga numbri kohta</p>
+    <input type="number" min="0" v-model="number">
+    <p>{{ data }}</p>
+
+  </div>
+</template>
+
+<script>
+const axios = require ('axios')
+export default {
+  name: 'Faktid',
+  data () {
+    return {
+      data: '',
+      number: 0
+    }
+  },
+  watch: {
+    number() {
+      if (this.number !== '' && this.number !== '') {
+      let url = `http://numbersapi.com/${this.number}`
+      
+      axios.get(url)
+      .then(response => {
+        this.data = response.data
+      })
+      .catch(error => {
+        console.log(error)
+      })
+      }
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+ 
+</style>
